ad1e74c03f958ed0286d00414b91d219053bd960
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-03-15 16:00:27 +0100

    Update gitignore
    Added images and resized_images to gitignore

--- Image_Resizer.py
+++ Image_Resizer.py
@@ -5,16 +5,16 @@ def webpToJpeg(string):
     im = Image.open(string).convert("RGB")
     im.save(string[:-4] + "jpg", "jpeg")
 
-string = "ExamplePics/0up1cmenb8ma1.webp"
-webpToJpeg(string)
+#string = "ExamplePics/0up1cmenb8ma1.webp"
+#webpToJpeg(string)
 
 def resize(image):
     size = 600
     resized_image = image.resize((size, size))
     return resized_image
 
-im = Image.open("ExamplePics/0up1cmenb8ma1.jpg")
-resize(im).save("ExamplePics/0up1cmenb8ma1_resized.jpg")
+#im = Image.open("ExamplePics/0up1cmenb8ma1.jpg")
+#resize(im).save("ExamplePics/0up1cmenb8ma1_resized.jpg")
 directory = "images"
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
