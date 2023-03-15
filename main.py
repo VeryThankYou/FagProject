e48ecf2668f5bee9bb910d8436e2910961f7496c
Frederik Winther Bæk <frederik.baek@hotmail.com>
2023-03-15 16:14:18 +0100

    Implemented main
    Adds script that will execute scripts for data downloading and resizing.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from DownloadRedditPictures import DownloadRedditPictures
+from Image_Resizer import resize_all
 
-redditPictures=DownloadRedditPictures(1000,600)
+size=600
+limit=100
+
+redditPictures=DownloadRedditPictures(limit,size)
 redditPictures.savePictures()
+resize_all(size)
