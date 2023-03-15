aeb4485e52dcf7e46607ae5f92936c35b3237425
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-03-15 16:05:26 +0100

    Updated resizer for dynamic size
    Included size as parameter, made method for resizing everything

--- Image_Resizer.py
+++ Image_Resizer.py
@@ -8,16 +8,16 @@ def webpToJpeg(string):
 #string = "ExamplePics/0up1cmenb8ma1.webp"
 #webpToJpeg(string)
 
-def resize(image):
-    size = 600
+def resize(image, size):
     resized_image = image.resize((size, size))
     return resized_image
 
 #im = Image.open("ExamplePics/0up1cmenb8ma1.jpg")
 #resize(im).save("ExamplePics/0up1cmenb8ma1_resized.jpg")
-directory = "images"
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    im = Image.open(f)
-    resize(im).save(os.path.join("resized_images", filename))
+def resize_all(size):
+    directory = "images"
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        im = Image.open(f)
+        resize(im, size).save(os.path.join("resized_images", filename))
