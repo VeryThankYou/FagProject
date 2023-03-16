7343e6ac5858cf53cefe299a375fd59e1f2ba5b2
Frederik Winther Bæk <frederik.baek@hotmail.com>
2023-03-16 13:49:27 +0100

    Changed some parameters

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from DownloadRedditPictures import DownloadRedditPictures
 from Image_Resizer import resize_all
 
 size=600
-limit=10
+limit=1000
 redditPictures=DownloadRedditPictures(limit,size)
 redditPictures.savePictures()
 resize_all(size)
