ef2fe7a57581697f5d6f329358b7c3dfed62e6f1
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-06-17 15:58:53 +0200

    Commented HFFeatureExtraction

--- ResNet/HFFeatureExtractor.py
+++ ResNet/HFFeatureExtractor.py
@@ -1,22 +1,18 @@
 import numpy as np
 import pandas as pd
-import PIL
 import transformers as tf
 import torch
 from PIL import Image
-import requests
 import sklearn.metrics as skm
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
-import matplotlib.pyplot as plt
-from torchvision import utils
 import os
 
+# Define a dataset class as a subclass of PyTorch's dataset class
 class CustomImageDataset(Dataset):
     def __init__(self, Xs, ys):
         self.img_labels = ys
         self.inputs = Xs
-
 
     def __len__(self):
         return len(self.img_labels)
@@ -26,19 +22,21 @@ class CustomImageDataset(Dataset):
         image["labels"] = torch.as_tensor(self.img_labels[idx]).float()
         return image
 
+# Load model and image processor from Huggingface
 processor = tf.AutoImageProcessor.from_pretrained("microsoft/resnet-50")
 model = tf.ResNetForImageClassification.from_pretrained("microsoft/resnet-50", num_labels = 1, ignore_mismatched_sizes = True).to("cuda")
-origDir = os.getcwd()
 
+# Change directory to get the dataset
+origDir = os.getcwd()
 os.chdir(origDir+"/Data")
+# Load dataset, add logupvotes
 df = pd.read_csv("submissions.csv")
 upvotes = df["Score"].to_numpy()
 logupvotes = np.log(upvotes+1)
 df["Logscore"] = logupvotes
 ids = df["ID"]
 
-
-
+# Rename id-column to the names of the images based on directory
 def rename_ids():
     newids = []
     for e in ids:
@@ -47,6 +45,8 @@ def rename_ids():
 
 names = rename_ids()
 df["Filename"] = names
+
+# Load input and output. For a full training change df1000 to df
 df1000 = df.iloc[:1000]
 X = [processor(Image.open(e)) for e in df1000["Filename"]]
 y = df1000["Logscore"].to_numpy()
@@ -54,12 +54,10 @@ dfnew = df.iloc[:10000]
 #X = [processor(Image.open(e)) for e in dfnew["Filename"]]
 #y = dfnew["Logscore"].to_numpy()
 
-BLpred = [np.mean(y) for i in range(len(y))]
-BLrmse = skm.mean_squared_error(y, BLpred, squared=False)
-print(BLrmse)
-
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
+# Define method for error stats for out regression
 def compute_metrics_for_regression(eval_pred):
     logits, labels = eval_pred
     labels = labels.reshape(-1, 1)
@@ -72,9 +70,10 @@ def compute_metrics_for_regression(eval_pred):
 
     return {"mse": mse, "rmse": rmse, "mae": mae, "r2": r2, "smape": smape}
 
-
+# Change back to ResNet directory
 os.chdir(origDir + "/ResNet")
 
+# Define training hyperparameters for our model
 training_args = tf.TrainingArguments(
     output_dir ='./resultsCNN',          
     num_train_epochs = 10,     
@@ -90,12 +89,13 @@ training_args = tf.TrainingArguments(
     save_strategy = "epoch",
 ) 
 
+# Put our training and test data into our custom class
 train_dataset = CustomImageDataset(X_train, y_train)
 valid_dataset = CustomImageDataset(X_test, y_test)
 
 
 
-# Call the Trainer
+# Define the Trainer
 trainer = tf.Trainer(
     model = model,                         
     args = training_args,                  
@@ -104,6 +104,6 @@ trainer = tf.Trainer(
     compute_metrics = compute_metrics_for_regression,     
 )
 
-
+# Train the model
 trainer.train()
 trainer.evaluate()
